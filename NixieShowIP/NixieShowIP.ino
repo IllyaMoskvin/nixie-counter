02a800b6224470e4b9f6f70f255d037d65460b2a
Illya Moskvin <ivmoskvin@gmail.com>
2019-08-25 22:30:59 -0500

    Implement alternating marquee for IPs
    This one alternates in the manner of the old marquee element, when its
    behavior is set to alternate. Kind of hard to read. Will try sliding,
    one direction right-to-left without alternating.

--- NixieShowIP/NixieShowIP.ino
+++ NixieShowIP/NixieShowIP.ino
@@ -8,13 +8,35 @@ const int dataPin = 12;
 const int clockPin = 14;
 const int oePin = 16;
 
-const unsigned long ipRefreshInterval = 5000;
-unsigned long ipRefreshedAt;
+const unsigned long ipCheckInterval = 5000;
+unsigned long ipCheckedAt;
 
-const unsigned long displayRefreshInterval = 500;
+const unsigned long displayRefreshInterval = 50;
 unsigned long displayRefreshedAt;
 
+bool shouldScroll = false;
+
+const unsigned long scrollRestInterval = 2500;
+const unsigned long scrollTickInterval = 75;
+unsigned long scrollUpdatedAt;
+
+const int SCROLL_REST_LEFT = 0;
+const int SCROLL_MOVE_LEFT = 1;
+const int SCROLL_REST_RIGHT = 2;
+const int SCROLL_MOVE_RIGHT = 3;
+
+int scrollState = SCROLL_REST_LEFT;
+bool isScrollResting = true;
+int scrollOffset = 0;
+
+int digits[6] = {0, 0, 0, 0, 0, 0};
+int dots[6] = {0, 0, 0, 0, 0, 0};
+
 String ip;
+String oldIp;
+
+int ipDigitCount;
+int ipCharCount;
 
 nixie_esp nixie(dataPin, clockPin);
 
@@ -38,23 +60,83 @@ void setup()
 
 void loop()
 {
-  if (millis() - ipRefreshInterval > ipRefreshedAt) {
+  if (millis() - ipCheckedAt > ipCheckInterval) {
     if (WiFi.status() == WL_CONNECTED) {
+      oldIp = ip;
       ip = WiFi.localIP().toString();
+
+      // Uncomment for testing:
+      // ip = String("192.168.123.245");
+      // ip = String("10.0.0.2");
+
+      if (ip != oldIp) {
+        ipDigitCount = 0;
+        ipCharCount = ip.length();
+
+        for (int i = 0; i < ipCharCount; i++) {
+          if (isDigit(ip[i])) {
+            ipDigitCount++;
+          }
+        }
+
+        shouldScroll = ipDigitCount > 6;
+
+        scrollState = SCROLL_REST_LEFT;
+        isScrollResting = true;
+        scrollOffset = 0;
+      }
+
       Serial.println(ip);
     }
-    
-    ipRefreshedAt = millis();
+
+    ipCheckedAt = millis();
+  }
+
+  if (shouldScroll && millis() - scrollUpdatedAt > (isScrollResting ? scrollRestInterval : scrollTickInterval)) {
+    switch (scrollState) {
+      case SCROLL_REST_LEFT:
+        scrollState = SCROLL_MOVE_LEFT;
+        isScrollResting = false;  
+        break;
+      case SCROLL_MOVE_LEFT:
+        if (scrollOffset + 6 == ipDigitCount) {
+          scrollState = SCROLL_REST_RIGHT;
+          isScrollResting = true;
+        } else {
+          scrollOffset += 1;
+        }
+        break;
+      case SCROLL_REST_RIGHT:
+        scrollState = SCROLL_MOVE_RIGHT;
+        isScrollResting = false;
+        break;
+      case SCROLL_MOVE_RIGHT:
+        if (scrollOffset == 0) {
+          scrollState = SCROLL_REST_LEFT;
+          isScrollResting = true;
+        } else {
+          scrollOffset -= 1;
+        }
+        break;
+    }
+
+    scrollUpdatedAt = millis();
   }
   
   if (millis() - displayRefreshedAt > displayRefreshInterval) {
-    displayIp();
-    
+    displayIp(ip, scrollOffset);
+
+    for(int i = 0; i < 6; i++) {
+      nixie.setDecimalPoint(i, dots[i]);
+    }
+
+    nixie.displayDigits(digits[0], digits[1], digits[2], digits[3], digits[4], digits[5]);
+
     displayRefreshedAt = millis();
   }
 }
 
-void displayIp()
+void displayIp(String ip, int offset)
 {
   int c = ip.length();
 
@@ -63,24 +145,27 @@ void displayIp()
   }
 
   for(int i = 0; i < 6; i++) {
-    nixie.setDecimalPoint(i, false);
+    digits[i] = BLANK;
+    dots[i] = false;
   }
 
-  int seg[6];
   int d = 0;
 
   for (int i = 0; i < c; i++) {
-    // Convert char to int by subtracting the ASCII value for zero
     if (isDigit(ip[i]) && d < 6) {
-      seg[d] = ip[i] - '0';
-      d++;
+      if (offset == 0) {
+        // Convert char to int by subtracting the ASCII value for zero
+        digits[d] = ip[i] - '0';
+        d++;
+      } else {
+        offset -= 1;
+      }
+      continue;
     }
 
     // For decimals, segment zero is 2nd tube from left
     if (ip[i] == '.' && d > 0 && d < 6) {
-      nixie.setDecimalPoint(d - 1, true);
+      dots[d - 1] = true;
     }
   }
-
-  nixie.displayDigits(seg[0], seg[1], seg[2], seg[3], seg[4], seg[5]);
 }
