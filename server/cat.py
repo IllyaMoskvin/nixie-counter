4c5bd9e76d28124a60059933130894fa685014cf
Illya Moskvin <ivmoskvin@gmail.com>
2021-10-30 11:55:10 -0500

    Fix `cat.py` to handle spaces in path

--- server/cat.py
+++ server/cat.py
@@ -4,7 +4,8 @@ import sys
 class ReadFileHTTPRequestHandler(nixie.NixieHTTPRequestHandler):
 
     def get_data(self):
-        with open(sys.argv[1], 'r') as file:
+        path = ' '.join(sys.argv[1:])
+        with open(path, 'r') as file:
             return int(file.read().replace('\n', ''))
 
 nixie.serve(ReadFileHTTPRequestHandler)
